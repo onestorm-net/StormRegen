2e4192bc8550404e02861efc35bf03b2eb569550
CdeJong <christiaan240@gmail.com>
2024-09-23 17:21:30 +0200

    Updated to Minecraft 1.21.1 and Java 21

--- StormRegen-Paper/src/main/java/net/onestorm/plugins/stormregen/paper/util/ItemStackUtil.java
+++ StormRegen-Paper/src/main/java/net/onestorm/plugins/stormregen/paper/util/ItemStackUtil.java
@@ -39,14 +39,13 @@ public class ItemStackUtil {
             List<Component> lore = new ArrayList<>();
             storageList.forEach(value -> {
                 Component line;
-                if (value instanceof String string) {
-                    line = MINI_MESSAGE.deserialize(string);
-                } else if (value instanceof Number number) {
-                    line = Component.text(String.valueOf(number));
-                } else if (value instanceof Boolean bool) {
-                    line = Component.text(bool);
-                } else {
-                    return; // continue
+                switch (value) {
+                    case String string -> line = MINI_MESSAGE.deserialize(string);
+                    case Number number -> line = Component.text(String.valueOf(number));
+                    case Boolean bool -> line = Component.text(bool);
+                    case null, default -> {
+                        return; // continue
+                    }
                 }
                 lore.add(line);
             });
